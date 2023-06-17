51878e085ba875219f16a7251052c89b8ecb5b81
Jang_Laptop <sdsd090811@gmail.com>
2023-06-17 17:15:57 +0900

    fix: import 누락 수정

--- src/main/java/com/mock/interviewer/api/service/chatgpt/OpenAIChatGPTService.java
+++ src/main/java/com/mock/interviewer/api/service/chatgpt/OpenAIChatGPTService.java
@@ -1,5 +1,7 @@
 package com.mock.interviewer.api.service.chatgpt;
 
+import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.service.OpenAiService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
@@ -8,18 +10,19 @@ import org.springframework.stereotype.Service;
 @Service
 @PropertySource("classpath:config.properties")
 public class OpenAIChatGPTService implements ChatGPTService{
+
     @Value("${openai.key}")
     private String key;
+
     @Override
     public String sendMessageToGPT(String talk) {
-//        OpenAiService service = new OpenAiService(key);
-//        CompletionRequest completionRequest = CompletionRequest.builder()
-//                .prompt(talk)
-//                .model("gpt-3.5-turbo")
-//                .echo(true)
-//                .maxTokens(3)
-//                .build();
-//        service.createCompletion(completionRequest).getChoices().forEach(System.out::println);
+        OpenAiService service = new OpenAiService(key);
+        CompletionRequest completionRequest = CompletionRequest.builder()
+                .prompt(talk)
+                .model("gpt-3.5-turbo")
+                .echo(true)
+                .build();
+        service.createCompletion(completionRequest).getChoices().forEach(System.out::println);
         return "";
     }
 }
